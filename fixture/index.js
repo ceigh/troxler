63364f502019f0d1a4735dfa342e3387e3bcbb7c
Artjom Löbsack <me@ceigh.com>
2020-12-13 20:49:50 +0500

    feat: add basic rendering

--- fixture/index.js
+++ fixture/index.js
@@ -1,4 +1,12 @@
-import { generateTroxlerImg } from '../src'
+import { drawTroxler } from '../src'
 
-const canvas = document.querySelector('canvas')
-generateTroxlerImg(canvas)
+window.addEventListener('load', () => {
+  const canvas = document.querySelector('canvas')
+  drawTroxler(canvas)
+
+  // redraw
+  canvas.addEventListener('click', () => {
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+    drawTroxler(canvas)
+  })
+})
--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
-export function generateTroxlerImg (canvas) {
-  console.log(canvas)
+const rndByte = () => Math.floor(Math.random() * 255)
+const rndColor = (alpha = 0.5) =>
+  `rgba(${rndByte()}, ${rndByte()}, ${rndByte()}, ${alpha})`
+
+function draw (canvas) {
+  // const { width: w, height: h } = canvas
+  const ctx = canvas.getContext('2d')
+  // console.log(ctx)
+
+  // for blending
+  ctx.filter = 'blur(1.5rem)'
+
+  ctx.fillStyle = rndColor()
+  ctx.fillRect(10, 10, 50, 50)
+  ctx.fillStyle = rndColor()
+  ctx.fillRect(30, 30, 50, 50)
+
+  /*
+  // to enlarge brightness
+  ctx.fillStyle = 'rgb(255, 255, 255, 0.5)'
+  ctx.fillRect(0, 0, w, h)
+  */
+}
+
+export function drawTroxler (canvas) {
+  if (!canvas) throw new Error('provide element')
+  if (!canvas.getContext) {
+    console.warn('this browser does not support canvas, ' +
+      'or the transferred item is not canvas')
+  } else draw(canvas)
 }
