21464db918c83b1eebf133ac73ac36954036d0da
Artjom Löbsack <me@ceigh.com>
2020-12-13 19:31:31 +0500

    feat: init project

---
+++ fixture/index.js
@@ -0,0 +1,4 @@
+import { generateTroxlerImg } from '../src'
+
+const canvas = document.querySelector('canvas')
+generateTroxlerImg(canvas)
---
+++ src/index.js
@@ -0,0 +1,3 @@
+export function generateTroxlerImg (canvas) {
+  console.log(canvas)
+}
