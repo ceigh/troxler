f5c66a73742c56471f1742d5308fb41d600fec2e
Artjom Löbsack <me@ceigh.com>
2020-12-14 23:44:00 +0500

    feat: add new options

--- fixture/index.js
+++ fixture/index.js
@@ -2,11 +2,17 @@ import { drawTroxler } from '../'
 
 window.addEventListener('load', () => {
   const canvas = document.querySelector('canvas')
-  drawTroxler(canvas)
+  const args = [canvas, {
+    // marker: true,
+    // brighter: false,
+    // bg: 'rgb(255, 255, 255)',
+    // worker: true
+  }]
+  drawTroxler(...args)
 
   // redraw
   canvas.addEventListener('click', () => {
     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-    drawTroxler(canvas)
+    drawTroxler(...args)
   })
 })
--- index.js
+++ index.js
@@ -1,20 +1,28 @@
+// helpers
 const rndTo = to => Math.floor(Math.random() * to)
 const rndByte = () => rndTo(255)
 const rndColor = (alpha = 0.3) =>
   `rgba(${rndByte()}, ${rndByte()}, ${rndByte()}, ${alpha})`
 
-function draw (canvas, marker) {
+// just draw on canvas
+function draw (canvas, options) {
   const { width: w, height: h } = canvas
   const side = Math.floor(Math.max(w, h) / 4)
   const ctx = canvas.getContext('2d')
   // console.log(ctx)
 
-  // fill white
-  ctx.fillStyle = 'rgb(255, 255, 255)'
-  ctx.fillRect(0, 0, w, h)
+  // fill bg
+  const { bg } = options
+  if (bg) {
+    ctx.fillStyle = bg
+    ctx.fillRect(0, 0, w, h)
+  }
 
   // for blending
+  // blur with canvas
   ctx.filter = 'blur(1.5rem)'
+  // blur with css
+  // if (!canvas.style.filter) canvas.style.filter = 'blur(1.5rem)'
 
   // random rectangles
   for (let i = side; i--;) {
@@ -22,30 +30,51 @@ function draw (canvas, marker) {
     ctx.fillRect(rndTo(w), rndTo(h), side, side)
   }
 
-  /*
-  // to enlarge brightness (extra)
-  ctx.fillStyle = 'rgb(255, 255, 255, 0.5)'
-  ctx.fillRect(0, 0, w, h)
-  */
+  // enlarge brightness
+  if (options.brighter) {
+    ctx.fillStyle = 'rgb(255, 255, 255, 0.5)'
+    ctx.fillRect(0, 0, w, h)
+  }
 
   // draw marker
-  if (!marker) return
-  const markerSide = Math.round(Math.max(w, h) / 100)
-  const markerSideHalf = Math.round(markerSide / 2)
-  const markerX = Math.round(w / 2 - markerSideHalf)
-  const markerY = Math.round(h / 2 - markerSideHalf)
-  ctx.filter = 'none'
-  ctx.fillStyle = 'rgb(0, 0, 0)'
-  ctx.fillRect(markerX, markerY, markerSide, markerSide)
-
-  // blur with css
-  // if (!canvas.style.filter) canvas.style.filter = 'blur(1.5rem)'
+  if (options.marker) {
+    const markerSide = Math.round(Math.max(w, h) / 100)
+    const markerSideHalf = Math.round(markerSide / 2)
+    const markerX = Math.round(w / 2 - markerSideHalf)
+    const markerY = Math.round(h / 2 - markerSideHalf)
+    ctx.filter = 'none'
+    ctx.fillStyle = 'rgb(0, 0, 0)'
+    ctx.fillRect(markerX, markerY, markerSide, markerSide)
+  }
 }
 
-export function drawTroxler (canvas, marker = true) {
+// move drawing to new thread
+function drawInWorker (canvas, options) {
+  if (!window.Worker) {
+    console.warn('this browser does not support web workers, ' +
+      'fallback to normal drawing')
+    draw(canvas, options)
+    return
+  }
+
+  // TODO: web workers
+  draw(canvas, options)
+}
+
+export function drawTroxler (canvas, {
+  bg = 'rgb(255, 255, 255)',
+  brighter = false,
+  marker = true,
+  worker = true
+} = {}) {
   if (!canvas) throw new Error('provide element')
   if (!canvas.getContext) {
     console.warn('this browser does not support canvas, ' +
       'or the transferred item is not canvas')
-  } else draw(canvas, marker)
+    return
+  }
+
+  const options = { bg, brighter, marker, worker }
+  if (worker) drawInWorker(canvas, options)
+  else draw(canvas, options)
 }
